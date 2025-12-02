431abff36f7cf68db5fa84fe3bb6c70af58529f6
Rifqiww <rifqirafiudin569@gmail.com>
2025-12-02 13:29:45 +0700

    🚧In Progress: Orders page

---
+++ src/pages/orders/orders.tsx
@@ -0,0 +1,13 @@
+const Orders = () => {
+    return (
+        <section className="mt-28">
+            <div>
+                <h1 className="text-white">
+                    Ini adalah page Orders
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default Orders
